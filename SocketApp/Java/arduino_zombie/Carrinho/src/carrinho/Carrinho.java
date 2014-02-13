569c138257b5f3233b5d5cb1267e8e7c567fa9d8
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-02-13 00:27:10 -0200

    ok

---
+++ SocketApp/Java/arduino_zombie/Carrinho/src/carrinho/Carrinho.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrinho;
+
+import com.igordeoliveira.dev.zombie.ArduinoZombieDefault;
+
+/**
+ *
+ * @author igor
+ */
+public class Carrinho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArduinoZombieDefault
+    }
+}
